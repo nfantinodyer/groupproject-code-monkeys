4d009fddd9e952ccf8d634a3d35e5216a49debac
Nicholas <n_fantinodyer@u.pacific.edu>
2021-10-29 11:28:13 -0700

    EntityType Class

---
+++ src/EntityType.java
@@ -0,0 +1,16 @@
+
+public enum EntityType {
+	BANANA, CHERRY, MANGO, BARREL, WALL, CHARACTER;
+	
+	public String toString() {
+		switch(this) {
+			case BANANA: return "banana";
+			case CHERRY: return "cherry";
+			case MANGO: return "mango";
+			case BARREL: return "barrel";
+			case WALL: return "wall";
+			case CHARACTER: return "character";
+		}
+		return "n/a";
+	}
+}
