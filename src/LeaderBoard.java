9a810371814b3c256bcae85047fa06096220789b
Nicholas <n_fantinodyer@u.pacific.edu>
2021-11-03 13:32:41 -0700

    Added a way to save previous data for scores.

--- src/LeaderBoard.java
+++ src/LeaderBoard.java
@@ -9,49 +9,52 @@ public class LeaderBoard {
 
 	LeaderBoard(String n,String m, int s){
 		
-		//Scores that will be on the leaderboard so it's not empty
-		easy.put(20, "jj");
-		easy.put(23, "hat");
-		easy.put(19, "iop");
-		easy.put(22, "koi");
-		easy.put(26, "gep");
 		
-		medium.put(35, "uop");
-		medium.put(37, "yike");
-		medium.put(31, "pet");
-		medium.put(32, "tate");
-		medium.put(30, "gom");
-		
-		hard.put(45, "kil");
-		hard.put(46, "huy");
-		hard.put(48, "ruy");
-		hard.put(38, "bad");
-		hard.put(47,"chris");
-		
-		
-		if(m=="easy")
-		{
-			easy.put(s,n);
-		}
-		else if(m=="medium")
-		{
-			medium.put(s,n);
-		}
-		else
-		{
-			hard.put(s,n);
-		}
 		
 		File f = new File("leaderboard.txt"); 
 		if(!f.exists())
 		{
 			makeFile();
+			//Scores that will be on the leaderboard so it's not empty
+			easy.put(20, "jj");
+			easy.put(23, "hat");
+			easy.put(19, "iop");
+			easy.put(22, "koi");
+			easy.put(26, "gep");
+			
+			medium.put(35, "uop");
+			medium.put(37, "yike");
+			medium.put(31, "pet");
+			medium.put(32, "tate");
+			medium.put(30, "gom");
+			
+			hard.put(45, "kil");
+			hard.put(46, "huy");
+			hard.put(48, "ruy");
+			hard.put(38, "bad");
+			hard.put(47,"chris");
+			
+			
+			if(m=="easy")
+			{
+				easy.put(s,n);
+			}
+			else if(m=="medium")
+			{
+				medium.put(s,n);
+			}
+			else
+			{
+				hard.put(s,n);
+			}
+			writeFile();
 		}
 		else
 		{
-			writeFile();
 			readFile();
+			writeFile();
 		}
+		readOnly();
 	}
 	
 	
@@ -73,7 +76,6 @@ public class LeaderBoard {
 		        
 
 		    output.close();
-		    System.out.println("File has been written");
 		}catch(IOException ioe) {}
 		
 
@@ -141,19 +143,68 @@ public class LeaderBoard {
 	{
 		try
 		{
-			int ch = 0;
-			FileReader fr=null;
-	        fr = new FileReader("leaderboard.txt");
-	  
-	       while ((ch=fr.read())!=-1)
-	       {
-	    	   System.out.print((char)ch);
-	       }
-				    
+			boolean nextv = false;
+			boolean nextk = false;
+			int key = 0;
+			int i=0;
+			String value = "";
+			Scanner sc2 = new Scanner(new File("leaderboard.txt"));
 			
-		    fr.close();
+			 while (sc2.hasNextLine()) {
+		            Scanner s2 = new Scanner(sc2.nextLine());
+		        while (s2.hasNext()) {
+		            String st = s2.next();
+		            
+		            if((st.equals("5.")||st.equals("4.")||st.equals("3.")||st.equals("2.")||st.equals("1.")))
+		            {
+		            	nextv=true;
+		            }
+		            else if(nextv)
+		            {
+		            	value = st;
+		            	nextv=false;
+		            	nextk=true;
+		            }
+		            else if(nextk)
+		            {
+		            	key=Integer.parseInt(st);
+		            	nextk=false;
+		            	 if(i<16)
+				            {
+				            	easy.put(key, value);
+				            }
+				            else if(i>15&&i<32)
+				            {
+				            	medium.put(key, value);
+				            }
+				            else
+				            {
+				            	hard.put(key, value);
+				            }
+		            }
+		            
+		           
+		            i++;
+		        }   
+	        }
+			 
+				   
 		}catch(IOException ioe) {}
 		
+	}
+	
+	public void readOnly()
+	{
+		try
+		{
+			FileReader fr = new FileReader("leaderboard.txt");
+			int ch = 0;
+			while ((ch=fr.read())!=-1)
+			{
+				System.out.print((char)ch);
+			}
+			fr.close();
+		}catch(IOException ioe) {}
 	}
 	
 }
