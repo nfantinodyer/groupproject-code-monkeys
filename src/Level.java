aecfd3ca44546d48f66aa894d47e5171ae8ca3db
John Sena <j_sena1@u.pacific.edu>
2021-11-03 11:09:04 -0700

    created empty methods for level

--- src/Level.java
+++ src/Level.java
@@ -1,4 +1,45 @@
 import java.util.*;
 public class Level {
+	String mapSize;
+	Map map;
+	int barrelMove;
+	Space winSpace;
+	
+	public Level createLevel(String s) {
+		return null;
+	}
+	public void setUpLevelEasy(){
+		
+	}
+	public void setUpLevelMed(){
+		
+	}
+	public void setUpLevelHard(){
+		
+	}
+	public Space getWin() {
+		return null;
+	}
+	public String toString() {
+		return null;
+	}
+	public void setWin(Space s) {
+		
+	}
+	public Boolean moveChara(Space s, int m){
+		return false;
+	}
+	public int timer() {
+		return 0;
+	}
+	public int moveBarrel(int i) {
+		return 0;
+	}
+	public int getMapSize() {
+		return 0;
+	}
+	
+	//to add
+	//incorpate keyboard listeners from interfacable.java
 
 }
