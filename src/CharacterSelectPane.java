abae2d2ce6a1473968688f4ff1fd0994ab1ad936
Nathan Ho <natha@10.15.140.157>
2021-11-10 12:10:38 -0800

    Renamed SomePane to CharacterSelectPane

---
+++ src/CharacterSelectPane.java
@@ -0,0 +1,54 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+
+public class CharacterSelectPane extends GraphicsPane {
+	// you will use program to get access to all of the GraphicsProgram calls
+	private MainApplication program; 
+	private GImage charOrang;
+	private GImage charGorilla;
+	private GImage charMonkey;
+	private GParagraph para;
+	private GButton rect;
+
+	public CharacterSelectPane(MainApplication app) {
+		this.program = app;
+		para = new GParagraph("Character Select", app.getWidth() / 2.3, app.getHeight() / 10);
+		para.setFont("Arial-26");
+		
+		rect = new GButton("Back", 25, 25, 60, 60);
+		rect.setFillColor(Color.RED);
+		
+		charMonkey = new GImage("Chimp_Cartoon.jpg", 250, 200);
+		charGorilla = new GImage("Gorilla_Cartoon.jpg", 650, 200);
+		charOrang = new GImage ("Orangutan_Cartoon.jpg", 950, 200);
+		
+		
+	}
+
+	@Override
+	public void showContents() {
+		program.add(para);
+		program.add(rect);
+		program.add(charOrang);
+		program.add(charGorilla);
+		program.add(charMonkey);
+	}
+
+	@Override
+	public void hideContents() {
+		program.remove(para);
+		program.remove(rect);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == rect) {
+			program.switchToMenu();
+		}
+		
+	}
+}
