d943e757420acd7eb21c643843cecc4d118790c3
Jacob Douma <j_douma1@u.pacific.edu>
2021-11-10 11:15:11 -0800

    Add LevelSelectPane for after CharaSelect

---
+++ src/LevelSelectPane.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+
+public class LevelSelectPane extends GraphicsPane {
+	// you will use program to get access to all of the GraphicsProgram calls
+	private MainApplication program; 
+
+	private GImage img;
+	private GParagraph para;
+	private GButton rect;
+
+	public LevelSelectPane(MainApplication app) {
+		this.program = app;
+		para = new GParagraph("Level Select", app.getWidth() / 2.3, app.getHeight() / 10);
+		para.setFont("Arial-26");
+		
+		rect = new GButton("Back", 25, 25, 60, 60);
+		rect.setFillColor(Color.RED);
+		
+		//img = new GImage("robot head.jpg", 0, 0);
+		
+	}
+
+	@Override
+	public void showContents() {
+		//program.add(img);
+		program.add(para);
+		program.add(rect);
+	}
+
+	@Override
+	public void hideContents() {
+		//program.remove(img);
+		program.remove(para);
+		program.remove(rect);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == rect) {
+			program.switchToMenu();
+		}
+		
+	}
+}
