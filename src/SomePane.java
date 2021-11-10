209e7e2231d32afc4260d4893a7f3460039d7cea
Nathan Ho <natha@10.15.140.157>
2021-11-10 12:05:48 -0800

    Added character images to the character select screen

--- src/SomePane.java
+++ src/SomePane.java
@@ -7,8 +7,13 @@ import acm.graphics.GObject;
 public class SomePane extends GraphicsPane {
 	// you will use program to get access to all of the GraphicsProgram calls
 	private MainApplication program; 
-
-	private GImage img;
+	
+	/*private String MONKEY = "Chimp_Cartoon.png";
+	private String GORILLA = "Gorilla_Cartoon.png;";
+	private String ORANGUTAN = "Orangutan_Cartoon.png"; */
+	private GImage charOrang;
+	private GImage charGorilla;
+	private GImage charMonkey;
 	private GParagraph para;
 	private GButton rect;
 
@@ -20,20 +25,24 @@ public class SomePane extends GraphicsPane {
 		rect = new GButton("Back", 25, 25, 60, 60);
 		rect.setFillColor(Color.RED);
 		
-		//img = new GImage("robot head.jpg", 0, 0);
+		charMonkey = new GImage("Chimp_Cartoon.jpg", 250, 200);
+		charGorilla = new GImage("Gorilla_Cartoon.jpg", 650, 200);
+		charOrang = new GImage ("Orangutan_Cartoon.jpg", 950, 200);
+		
 		
 	}
 
 	@Override
 	public void showContents() {
-		//program.add(img);
 		program.add(para);
 		program.add(rect);
+		program.add(charOrang);
+		program.add(charGorilla);
+		program.add(charMonkey);
 	}
 
 	@Override
 	public void hideContents() {
-		//program.remove(img);
 		program.remove(para);
 		program.remove(rect);
 	}
