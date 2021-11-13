60662f8ca987f84e0c42450f0c9a94e9cee2e679
Jacob Douma <j_douma1@u.pacific.edu>
2021-11-12 21:35:27 -0800

    Added level images back to levelSelectPane. Implemented mouselistener. Need to adjust so game start when "NEXT" is pressed

--- src/LevelSelectPane.java
+++ src/LevelSelectPane.java
@@ -11,12 +11,14 @@ public class LevelSelectPane extends GraphicsPane {
 	
 	private GParagraph para;
 	private GButton rect;
-	private GLabel easyLabel = new GLabel("Easy", 225, 200);
+	private GLabel easyLabel = new GLabel("Easy", 232, 200);
 	private GLabel mediumLabel = new GLabel("Medium", 725, 200);
-	private GLabel hardLabel = new GLabel("Hard", 1225, 200);
+	private GLabel hardLabel = new GLabel("Hard", 1232, 200);
 	private GImage easyLevel;
 	private GImage mediumLevel;
 	private GImage hardLevel;
+	private GButton levelSelect = new GButton("SELECTED", 0, 0, 0, 0);
+	private GButton next = new GButton("NEXT", 1400, 700, 60, 60);
 
 	public LevelSelectPane(MainApplication app) {
 		this.program = app;
@@ -26,9 +28,9 @@ public class LevelSelectPane extends GraphicsPane {
 		rect = new GButton("Back", 25, 25, 60, 60);
 		rect.setFillColor(Color.RED);
 		
-		//easyLevel = new GImage("EasyMap.png", 225, 250);
-		//mediumLevel = new GImage("MediumMap.png", 725, 250);
-		//hardLevel = new GImage("HardMap.png", 1225, 250);
+		easyLevel = new GImage("EasyMap.png", 130, 250);
+		mediumLevel = new GImage("MediumMap.png", 630, 255);
+		hardLevel = new GImage("HardMap.png", 1130, 275);
 		
 		easyLabel.setFont("Arial-26");
 		easyLabel.setColor(Color.YELLOW);
@@ -47,9 +49,9 @@ public class LevelSelectPane extends GraphicsPane {
 		program.add(easyLabel);
 		program.add(mediumLabel);
 		program.add(hardLabel);
-		//program.add(easyLevel);
-		//program.add(mediumLevel);
-		//program.add(hardLevel);
+		program.add(easyLevel);
+		program.add(mediumLevel);
+		program.add(hardLevel);
 	}
 
 	@Override
@@ -59,17 +61,45 @@ public class LevelSelectPane extends GraphicsPane {
 		program.remove(easyLabel);
 		program.remove(mediumLabel);
 		program.remove(hardLabel);
-		//program.remove(easyLevel);
-		//program.remove(mediumLevel);
-		//program.remove(hardLevel);
+		program.remove(easyLevel);
+		program.remove(mediumLevel);
+		program.remove(hardLevel);
+		program.remove(levelSelect);
+		program.remove(next);
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
+		
+		if (levelSelect.isVisible()) {
+			program.remove(levelSelect);
+			program.remove(next);
+		}
+		if (obj == easyLevel) {
+			levelSelect = new GButton("SELECTED", 230, 575, 100, 50);
+		}
+		if (obj == mediumLevel) {
+			levelSelect = new GButton("SELECTED", 730, 575, 100, 50);
+		}
+		if (obj == hardLevel) {
+			levelSelect = new GButton("SELECTED", 1230, 575, 100, 50);
+		}
+		
+		levelSelect.setFillColor(Color.RED);
+		next.setFillColor(Color.RED);
+		program.add(levelSelect);
+		program.add(next);
+		
+		if (levelSelect.isVisible()) {
+			program.add(next);
+		}
+		if (obj == next) {
+			//START LEVEL
+			program.switchToMenu();
+		}
 		if (obj == rect) {
 			program.switchToCharSelect();
 		}
-		
 	}
 }
