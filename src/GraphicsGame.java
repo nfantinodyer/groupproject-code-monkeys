3234348f127b17a14e0a06138e0abc5df96f624a
Jacob Douma <j_douma1@u.pacific.edu>
2021-11-17 14:50:43 -0800

    Added winningSpace

--- src/GraphicsGame.java
+++ src/GraphicsGame.java
@@ -1,9 +1,10 @@
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 import acm.graphics.GLabel;
 import acm.graphics.GLine;
-import acm.graphics.GObject;
+import acm.graphics.GRect;
 
 public class GraphicsGame extends GraphicsPane {
 	// you will use program to get access to all of the GraphicsProgram calls
@@ -14,6 +15,7 @@ public class GraphicsGame extends GraphicsPane {
 	
 	private GLabel lives = new GLabel("LIVES:", 10, 25);
 	private GLabel score = new GLabel("SCORE:", 1250, 25);
+	GRect winSpace;
 	
 	private String s = "";
 	
@@ -42,8 +44,13 @@ public class GraphicsGame extends GraphicsPane {
 		s="hard";
 	}
 	
-	private void setupLevel(String s) {
-		drawGridLines(s);
+	private void drawWinningSpace() {
+		int winRow = level.getWin().getRow();
+		int winCol = level.getWin().getCol();
+		
+		winSpace = new GRect(winRow * spaceWidth(), winCol * spaceHeight());
+		winSpace.setFillColor(Color.RED);
+		program.add(winSpace);
 	}
 	
 	private void drawGridLines(String s) {
@@ -105,6 +112,7 @@ public class GraphicsGame extends GraphicsPane {
 		program.add(lives);
 		program.add(score);
 		drawGridLines(s);
+		drawWinningSpace();
 	}
 
 	@Override
@@ -112,6 +120,7 @@ public class GraphicsGame extends GraphicsPane {
 		program.remove(lives);
 		program.remove(score);
 		removeGridLines();
+		program.remove(winSpace);
 	}
 
 	@Override
