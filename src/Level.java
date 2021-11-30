cffd320bf12aa3b503526d4bf85da70a48915a50
John Sena <j_sena1@u.pacific.edu>
2021-11-30 14:57:30 -0800

    setting up hard level's wall reimplmetation

--- src/Level.java
+++ src/Level.java
@@ -264,244 +264,183 @@ public class Level extends GraphicsProgram implements EventListener, ActionListe
 		//player
 		map.addEntity(EntityType.CHARACTER, 0, 0, false);
 		
+		//0 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 0,i, false);
+				walls.add(new Entity(EntityType.WALL, 0,i, false));	
+			}
+		}
+		//1 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 1,i, false);
+				walls.add(new Entity(EntityType.WALL, 1,i, false));	
+			}
+		}
+		//2 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 2,i, false);
+				walls.add(new Entity(EntityType.WALL, 2,i, false));	
+			}
+		}
+		//3 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 3,i, false);
+				walls.add(new Entity(EntityType.WALL, 3,i, false));	
+			}
+		}
+		//4 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 4,i, false);
+				walls.add(new Entity(EntityType.WALL, 4,i, false));	
+			}
+		}
+		//5 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 5,i, false);
+				walls.add(new Entity(EntityType.WALL, 5,i, false));	
+			}
+		}
+		//6 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 6,i, false);
+				walls.add(new Entity(EntityType.WALL, 6,i, false));	
+			}
+		}
+		//7 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 7,i, false);
+				walls.add(new Entity(EntityType.WALL, 7,i, false));	
+			}
+		}
+		//8 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 8,i, false);
+				walls.add(new Entity(EntityType.WALL, 8,i, false));	
+			}
+		}
+		//9 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 9,i, false);
+				walls.add(new Entity(EntityType.WALL, 9,i, false));	
+			}
+		}
+		//10 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 10,i, false);
+				walls.add(new Entity(EntityType.WALL, 0,i, false));	
+			}
+		}
+		//11 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 11,i, false);
+				walls.add(new Entity(EntityType.WALL, 11,i, false));	
+			}
+		}
+		//12 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 12,i, false);
+				walls.add(new Entity(EntityType.WALL, 12,i, false));	
+			}
+		}
+		//13 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 13,i, false);
+				walls.add(new Entity(EntityType.WALL, 13,i, false));	
+			}
+		}
+		//14 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 14,i, false);
+				walls.add(new Entity(EntityType.WALL, 14,i, false));	
+			}
+		}
+		//15 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 15,i, false);
+				walls.add(new Entity(EntityType.WALL, 15,i, false));	
+			}
+		}
+		//16 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 16,i, false);
+				walls.add(new Entity(EntityType.WALL, 16,i, false));	
+			}
+		}
+		//17 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 17,i, false);
+				walls.add(new Entity(EntityType.WALL, 17,i, false));	
+			}
+		}
+		//18 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 18,i, false);
+				walls.add(new Entity(EntityType.WALL, 18,i, false));	
+			}
+		}
+		//19 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 19,i, false);
+				walls.add(new Entity(EntityType.WALL, 19,i, false));	
+			}
+		}
+		//20 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 20,i, false);
+				walls.add(new Entity(EntityType.WALL, 20,i, false));	
+			}
+		}
+		//21 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 21,i, false);
+				walls.add(new Entity(EntityType.WALL, 21,i, false));	
+			}
+		}
+		//22 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 22,i, false);
+				walls.add(new Entity(EntityType.WALL, 22,i, false));	
+			}
+		}
+		//23 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 23,i, false);
+				walls.add(new Entity(EntityType.WALL, 23,i, false));	
+			}
+		}
+		//24 hori
+		for (int i = 0; i < 25; i++) {
+			if(true) {
+				map.addEntity(EntityType.WALL, 24,i, false);
+				walls.add(new Entity(EntityType.WALL, 24,i, false));	
+			}
+		}
 		//WALLS
-		//Three Horizontal 
-		for(int i = 1; i < 3; i++) { 
-			map.addEntity(EntityType.WALL, i, 0, false);
-			map.addEntity(EntityType.WALL, 7+i, 0, false);
-			map.addEntity(EntityType.WALL, 21+i, 1, false);
-			map.addEntity(EntityType.WALL, 13+i, 1, false);
-			map.addEntity(EntityType.WALL, 11+i, 2, false);
-			map.addEntity(EntityType.WALL, 3+i, 4, false);
-			map.addEntity(EntityType.WALL, 11+i, 4, false);
-			map.addEntity(EntityType.WALL, 19+i, 4, false);
-			map.addEntity(EntityType.WALL, 1+i, 6, false);
-			map.addEntity(EntityType.WALL, 9+i, 8, false);
-			map.addEntity(EntityType.WALL, 13+i, 9, false);
-			map.addEntity(EntityType.WALL, 13+i, 10, false);
-			map.addEntity(EntityType.WALL, 21+i, 10, false);
-			map.addEntity(EntityType.WALL, 19+i, 12, false);
-			map.addEntity(EntityType.WALL, 21+i, 14, false);
-			map.addEntity(EntityType.WALL, 13+i, 16, false);
-			map.addEntity(EntityType.WALL, 11+i, 17, false);
-			map.addEntity(EntityType.WALL, 13+i, 19, false);
-			map.addEntity(EntityType.WALL, 21+i, 24, false);
-			//walls vector adding
-			walls.add(new Entity(EntityType.WALL, i, 0, false));
-			walls.add(new Entity(EntityType.WALL, 7+i, 0, false));
-			walls.add(new Entity(EntityType.WALL, 21+i, 1, false));
-			walls.add(new Entity(EntityType.WALL, 13+i, 1, false));
-			walls.add(new Entity(EntityType.WALL, 11+i, 2, false));
-			walls.add(new Entity(EntityType.WALL, 3+i, 4, false));
-			walls.add(new Entity(EntityType.WALL, 11+i, 4, false));
-			walls.add(new Entity(EntityType.WALL, 19+i, 4, false));
-			walls.add(new Entity(EntityType.WALL, 1+i, 6, false));
-			walls.add(new Entity(EntityType.WALL, 9+i, 8, false));
-			walls.add(new Entity(EntityType.WALL, 13+i, 9, false));
-			walls.add(new Entity(EntityType.WALL, 13+i, 10, false));
-			walls.add(new Entity(EntityType.WALL, 21+i, 10, false));
-			walls.add(new Entity(EntityType.WALL, 19+i, 12, false));
-			walls.add(new Entity(EntityType.WALL, 21+i, 14, false));
-			walls.add(new Entity(EntityType.WALL, 13+i, 16, false));
-			walls.add(new Entity(EntityType.WALL, 11+i, 17, false));
-			walls.add(new Entity(EntityType.WALL, 13+i, 19, false));
-			walls.add(new Entity(EntityType.WALL, 21+i, 24, false));
-		}
-		//Three Vertical
-		for(int i = 0; i < 3; i++) {
-			map.addEntity(EntityType.WALL, 3, 1+i, false);
-			map.addEntity(EntityType.WALL, 5, 1+i, false);
-			map.addEntity(EntityType.WALL, 11, 5+i, false);
-			map.addEntity(EntityType.WALL, 13, 5+i, false);
-			map.addEntity(EntityType.WALL, 9, 9+i, false);
-			map.addEntity(EntityType.WALL, 11, 9+i, false);
-			map.addEntity(EntityType.WALL, 19, 9+i, false);
-			map.addEntity(EntityType.WALL, 19, 14+i, false);
-			map.addEntity(EntityType.WALL, 17, 16+i, false);
-			map.addEntity(EntityType.WALL, 7, 16+i, false);
-			map.addEntity(EntityType.WALL, 13, 20+i, false);
-			map.addEntity(EntityType.WALL, 21, 22+i, false);
-			//walls vector adding
-			walls.add(new Entity(EntityType.WALL, 3, 1+i, false));
-			walls.add(new Entity(EntityType.WALL, 5, 1+i, false));
-			walls.add(new Entity(EntityType.WALL, 11, 5+i, false));
-			walls.add(new Entity(EntityType.WALL, 13, 5+i, false));
-			walls.add(new Entity(EntityType.WALL, 9, 9+i, false));
-			walls.add(new Entity(EntityType.WALL, 11, 9+i, false));
-			walls.add(new Entity(EntityType.WALL, 19, 9+i, false));
-			walls.add(new Entity(EntityType.WALL, 19, 14+i, false));
-			walls.add(new Entity(EntityType.WALL, 17, 16+i, false));
-			walls.add(new Entity(EntityType.WALL, 7, 16+i, false));
-			walls.add(new Entity(EntityType.WALL, 13, 20+i, false));
-			walls.add(new Entity(EntityType.WALL, 21, 22+i, false));
-		}
-		
-		//Four Vertical
-		for(int i = 0; i < 4; i++) 
-			{
-			map.addEntity(EntityType.WALL, 19, i, false);
-			map.addEntity(EntityType.WALL, 7, 1+i, false);
-			map.addEntity(EntityType.WALL, 1, 2+i, false);
-			map.addEntity(EntityType.WALL, 23, 2+i, false);
-			map.addEntity(EntityType.WALL, 5, 8+i, false);
-			map.addEntity(EntityType.WALL, 3, 10+i, false);
-			map.addEntity(EntityType.WALL, 23, 21+i, false);
 			
-			walls.add(new Entity(EntityType.WALL, 19, i, false));
-			walls.add(new Entity(EntityType.WALL, 7, 1+i, false));
-			walls.add(new Entity(EntityType.WALL, 1, 2+i, false));
-			walls.add(new Entity(EntityType.WALL, 23, 2+i, false));
-			walls.add(new Entity(EntityType.WALL, 5, 8+i, false));
-			walls.add(new Entity(EntityType.WALL, 3, 10+i, false));
-			walls.add(new Entity(EntityType.WALL, 23, 21+i, false));
-		}
-		
-		//Other
-		for(int i = 0; i < 2; i++) {
-			map.addEntity(EntityType.WALL, 11, i, false);
-			walls.add(new Entity(EntityType.WALL, 11, i, false));
-		}
-		for(int i = 0; i < 7; i++) {
-			map.addEntity(EntityType.WALL, 15, 2+i, false);
-			walls.add(new Entity(EntityType.WALL, 15, 2+i, false));
-		}
-		for(int i = 0; i < 6; i++) {
-			map.addEntity(EntityType.WALL, 9, 1+i, false);
-			walls.add(new Entity(EntityType.WALL, 9, 1+i, false));
-		}
-		for(int i = 0; i < 4; i++) {
-			map.addEntity(EntityType.WALL, 5+i, 6, false);
-			walls.add(new Entity(EntityType.WALL, 5+i, 6, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 19+i, 6, false);
-			walls.add(new Entity(EntityType.WALL, 19+i, 6, false));
-		}
-		for(int i = 0; i < 14; i++) {
-			map.addEntity(EntityType.WALL, 17, 1+i, false);
-			walls.add(new Entity(EntityType.WALL, 17, 1+i, false));
-		}
-		for(int i = 0; i < 2; i++) {
-			map.addEntity(EntityType.WALL, 21, 2+i, false);
-			walls.add(new Entity(EntityType.WALL, 21, 2+i, false));
-		}
-		for(int i = 0; i < 2; i++) {
-			map.addEntity(EntityType.WALL, 3, 7+i, false);
-			walls.add(new Entity(EntityType.WALL, 3, 7+i, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 7, 7+i, false);
-			walls.add(new Entity(EntityType.WALL, 7, 7+i, false));
-		}
-		for(int i = 0; i < 3; i++)	{
-			map.addEntity(EntityType.WALL, i, 8, false);
-			walls.add(new Entity(EntityType.WALL, i, 8, false));
-		}
-		for(int i = 0; i < 10; i++) {
-			map.addEntity(EntityType.WALL, 1, 9+i, false);
-			walls.add(new Entity(EntityType.WALL, 1, 9+i, false));
-		}
-		for(int i = 0; i < 14; i++) {
-			map.addEntity(EntityType.WALL, 3+i, 14, false);
-			walls.add(new Entity(EntityType.WALL, 3+i, 14, false));
-		}
-		for(int i = 0; i < 5; i++) 	{
-			map.addEntity(EntityType.WALL, 5+i, 12, false);
-			walls.add(new Entity(EntityType.WALL, 5+i, 12, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 11+i, 12, false);
-			walls.add(new Entity(EntityType.WALL, 11+i, 12, false));
-		}
-		for(int i = 0; i < 6; i++) {
-			map.addEntity(EntityType.WALL, 19+i, 8, false);
-			walls.add(new Entity(EntityType.WALL, 19+i, 8, false));
-		}
-		for(int i = 0; i < 2; i++) {
-			map.addEntity(EntityType.WALL, 23+i, 12, false);
-			walls.add(new Entity(EntityType.WALL, 23+i, 12, false));
-		}
-		for(int i = 0; i < 4; i++) {
-			map.addEntity(EntityType.WALL, 3+i, 17, false);
-			walls.add(new Entity(EntityType.WALL, 3+i, 17, false));
-		}
-		for(int i = 0; i < 4; i++) {
-			map.addEntity(EntityType.WALL, 20+i, 17, false);
-			walls.add(new Entity(EntityType.WALL, 20+i, 17, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 0, 20+i, false);
-			walls.add(new Entity(EntityType.WALL, 0, 20+i, false));
-		}
-		for(int i = 0; i < 6; i++) {
-			map.addEntity(EntityType.WALL, 2, 18+i, false);
-			walls.add(new Entity(EntityType.WALL, 2, 18+i, false));
-		}
-		for(int i = 0; i < 6; i++) {
-			map.addEntity(EntityType.WALL, 4, 18+i, false);
-			walls.add(new Entity(EntityType.WALL, 4, 18+i, false));
-		}
-		for(int i = 0; i < 2; i++) {
-			map.addEntity(EntityType.WALL, 5+i, 18, false);
-			walls.add(new Entity(EntityType.WALL, 4, 18+i, false));
-		}
-		for(int i = 0; i < 6; i++) {
-			map.addEntity(EntityType.WALL, 9, 15+i, false);
-			walls.add(new Entity(EntityType.WALL, 9, 15+i, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 11, 18+i, false);
-			walls.add(new Entity(EntityType.WALL, 11, 18+i, false));
-		}
-		for(int i = 0; i < 7; i++) {
-			map.addEntity(EntityType.WALL, 18+i, 18, false);
-			walls.add(new Entity(EntityType.WALL, 18+i, 18, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 6, 20+i, false);
-			walls.add(new Entity(EntityType.WALL, 6, 20+i, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 8, 20+i, false);
-			walls.add(new Entity(EntityType.WALL, 8, 20+i, false));
-		}
-		for(int i = 0; i < 5; i++) {
-			map.addEntity(EntityType.WALL, 15, 20+i, false);
-			walls.add(new Entity(EntityType.WALL, 15, 20+i, false));
-		}
-		for(int i = 0; i < 7; i++) {
-			map.addEntity(EntityType.WALL, 17+i, 20, false);
-			walls.add(new Entity(EntityType.WALL, 17+i, 20, false));
-		}
-		for(int i = 0; i < 4; i++) {
-			map.addEntity(EntityType.WALL, 16+i, 23, false);
-			walls.add(new Entity(EntityType.WALL, 16+i, 23, false));
-		}
-		for(int i = 0; i < 3; i++) {
-			map.addEntity(EntityType.WALL, 16+i, 24, false);
-			walls.add(new Entity(EntityType.WALL, 16+i, 24, false));
-		}
-		for(int i = 0; i < 2; i++) {
-			map.addEntity(EntityType.WALL, 10, 23+i, false);
-			walls.add(new Entity(EntityType.WALL, 10, 23+i, false));
-		}
-		for(int i = 0; i < 2; i++) {
-			map.addEntity(EntityType.WALL, 12+i, 24, false);
-			walls.add(new Entity(EntityType.WALL, 12+i, 24, false));
-		}
-		
-		//Singles
-		map.addEntity(EntityType.WALL, 23, 11, false);
-		map.addEntity(EntityType.WALL, 21, 14, false);
-		map.addEntity(EntityType.WALL, 11, 16, false);
-		map.addEntity(EntityType.WALL, 22, 24, false);
-		map.addEntity(EntityType.WALL, 23, 15, false);
-		
-		walls.add(new Entity(EntityType.WALL, 23, 11, false));
-		walls.add(new Entity(EntityType.WALL, 21, 14, false));
-		walls.add(new Entity(EntityType.WALL, 11, 16, false));
-		walls.add(new Entity(EntityType.WALL, 22, 24, false));
-		walls.add(new Entity(EntityType.WALL, 23, 15, false));
 	}
 	public Space getWin() {
 		return winSpace;
