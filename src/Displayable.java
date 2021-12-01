bae5696aab5951a574cae6797518d70a2aa35618
Nicholas <n_fantinodyer@u.pacific.edu>
2021-12-01 11:45:59 -0800

    Removed setlevel in displayable

--- src/Displayable.java
+++ src/Displayable.java
@@ -4,7 +4,7 @@
 public interface Displayable {
 	public void showContents();
 	public void hideContents();
-	public void setLevelEasy();
-	public void setLevelMed();
-	public void setLevelHard();
+	//public void setLevelEasy();
+	//public void setLevelMed();
+	//public void setLevelHard();
 }
